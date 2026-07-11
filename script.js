71148fb0e108404949787520bc08b2ce244ecdd1
rosaliyaax-glitch <rosaliyaax@gmail.com>
2026-07-11 10:42:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -164,62 +164,50 @@ envelope.addEventListener("click",()=>{
 
 const message = `hey, lil teddy bear 🧸💚
 
-
 hehe you actually found the secret thing.
-
 
 first of all... yo 😭 bahaha
 
-
 i know A Levels have been stressing you out a lot lately and your brain probably has like 500 tabs open at the same time 💀 lolol
-
 
 mehh idiot, i need you to remember something okay?
 
-
 you're doing better than you think.
-
 
 i know sometimes it feels like there's too much to do and everything is just piling up, but i see how much effort you put in. i see you trying, even on the days when you feel tired or unsure.
 
-
 so here's your tiny reminder:
-
 
 stop being so hard on urself harida 🫵😭
 
-
 take breaks when you need them.
-
 
 don't be so mean to yourself yea? only I get to do that :3
 
-
 and remember that one stressful day doesn't decide your whole future.
-
 
 i'm always cheering for you, even when you don't realize you need someone cheering.
 
-
 also btw...
-
 
 you better not underestimate yourself because you're actually so much more capable than you think 🤨💚
 
-
 okay i'll stop being cheesy now before this becomes a whole novel 💀
-
 
 just know that i care about you a lot.
 
-
 i wuv u sooo much, harida?
-
 
 more than yk 🥺❤️
 
-
 mwahh 🧸💗`;
+
+
+
+
+
+
+
 
 
 
